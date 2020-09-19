8e110c09cee78b071a946064a616120f5f53cf6e
lenovo <lenovo@DESKTOP-E2NK6C2>
2020-09-19 13:10:53 -0700

    adding myapp

---
+++ MyApplication/src/com/dxc/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.dxc;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("Hello World");
+		
+			
+			System.out.print("This is Java projject ");
+		
+
+	}
+
+}
